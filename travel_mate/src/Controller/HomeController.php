c2d7f78ac87249b037830ed46e645b5aa293faaa
Benjamin Gleitz <gleitz.benjamin@gmail.com>
2021-12-15 18:10:40 +0100

    dynamic country searchbar

--- travel_mate/src/Controller/HomeController.php
+++ travel_mate/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,10 +17,13 @@ class HomeController extends AbstractController
      * @Route(name="index")
      * @return Response
      */
-    public function index(): Response
+    public function index(CountryRepository $countryRepository): Response
     {
+
+        $countries = $countryRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'countries' => $countries,
         ]);
     }
 }
